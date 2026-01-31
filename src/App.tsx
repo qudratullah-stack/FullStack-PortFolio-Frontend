dbeec76d6c1150a64eb28dd270d8144710ec70d5
QudratUllah <qudratullahmasoom6688@gmail.com>
2026-01-31 11:52:10 +0500

    setup React + Ts + Tailwindcss project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './style/App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-8xl font-bold text-shadow-fuchsia-900'>hello world</h1>
+      
+    </>
+  )
+}
+
+export default App
