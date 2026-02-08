2abdffc6eaf642db6753545df6da8ade2e5d8d41
QudratUllah <qudratullahmasoom6688@gmail.com>
2026-02-08 16:28:43 +0500

    added the login and fetch the data to backend

--- src/authantications/Login.tsx
+++ src/authantications/Login.tsx
@@ -1,51 +1,96 @@
 import MyContext from "../context/CreateContext";
-import { useContext } from "react";
-import {  LucideMail, LucideLock, LucideUserPlus } from 'lucide-react';
-
+import { useContext , useState} from "react";
+import { LucideMail, LucideLock, LucideLogIn, LucideArrowRight } from 'lucide-react';
+import { useNavigate } from "react-router-dom";
+import Alert from "../componenets/partials/Alert";
+import Loading from "../componenets/partials/Loading";
+import axios from "axios";
 function Login() {
-   const { darkMode } = useContext(MyContext);
-
+  const { darkMode, setAlert, alert, setSuccess, success, loader, setloader } = useContext(MyContext);
+const navigate = useNavigate()
+const [Email, setEmail] = useState('')
+const [Password, setPassword] = useState('')
+const [emailBorder, setemailborder] = useState(false)
   const inputStyle = `w-full p-3 pl-10 rounded-xl border outline-none transition-all duration-300 text-sm md:text-base
   ${
     darkMode
-      ? "bg-gray-800/50 border-gray-700 text-white focus:border-cyan-500 focus:ring-2 focus:ring-cyan-500/20"
+      ? "bg-gray-800/50 border-gray-700 text-white focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20"
       : "bg-white border-gray-200 text-gray-900 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/20"
   }`;
+const saveLogin = async()=>{
+  try{
+    setloader(true)
+    await axios.post('http://localhost:9000/auth/login',{
+        email: Email,
+        password: Password
+    })
+    setAlert(true)
+    setSuccess('Account Maked Successfully')
+  }catch(err){
+    setAlert(true)
+    setSuccess('Invalid credentials')
+  }finally{
+    setloader(false)
+  }
+}
+const handleLogin = ()=>{
+   let inputtextmatch = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+       if(!inputtextmatch.test(Email)){
+        setemailborder(true)
+        setAlert(true)
+        setSuccess('Please enter a valid email address')
+        return
+      }
+  saveLogin()
+  setEmail('')
+  setPassword('')
+}
   return (
-   <>
-     <div className={`min-h-screen flex items-center justify-center p-6 transition-colors duration-500 ${darkMode ? 'bg-gray-950' : 'bg-gray-50'}`}>
+    <>
+    {alert && <Alert message={success} darkMode={darkMode} />}
+    <div className={`min-h-screen flex items-center justify-center p-6 transition-colors duration-500 ${darkMode ? 'bg-gray-950' : 'bg-gray-50'}`}>
       <div className={`w-full max-w-md p-8 rounded-3xl shadow-2xl transform transition-all duration-500 ${darkMode ? 'bg-gray-900 border border-gray-800' : 'bg-white border border-gray-100'}`}>
-
+        
+        {/* Header */}
         <div className="text-center mb-8">
-          <div className={`inline-flex p-4 rounded-2xl mb-4 ${darkMode ? 'bg-cyan-500/10 text-cyan-500' : 'bg-blue-500/10 text-blue-600'}`}>
-            <LucideUserPlus size={32} />
+          <div className={`inline-flex p-4 rounded-2xl mb-4 ${darkMode ? 'bg-blue-500/10 text-blue-500' : 'bg-blue-600/10 text-blue-600'}`}>
+            <LucideLogIn size={32} />
           </div>
-          <h2 className={`text-3xl font-bold ${darkMode ? 'text-white' : 'text-gray-900'}`}>Create Account</h2>
-          <p className={`mt-2 text-sm ${darkMode ? 'text-gray-400' : 'text-gray-500'}`}>Join Qudrat's Portfolio Community</p>
+          <h2 className={`text-3xl font-bold ${darkMode ? 'text-white' : 'text-gray-900'}`}>Welcome Back</h2>
+          <p className={`mt-2 text-sm ${darkMode ? 'text-gray-400' : 'text-gray-500'}`}>Please enter your details to sign in</p>
         </div>
 
         <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+     
           <div className="relative">
             <LucideMail className="absolute left-3 top-3.5 text-gray-400" size={18} />
-            <input type="email" placeholder="Email Address" className={inputStyle} required />
+            <input type="email" placeholder="Email Address" value={Email} onChange={(e)=> {setEmail(e.target.value)}}  className={`${inputStyle} ${emailBorder?'border-red-600':'border-gray-700'}`} required />
           </div>
           <div className="relative">
             <LucideLock className="absolute left-3 top-3.5 text-gray-400" size={18} />
-            <input type="password" placeholder="Password" className={inputStyle} required />
+            <input type="password" placeholder="Password" value={Password} onChange={(e)=>{setPassword(e.target.value)}} className={inputStyle} required />
           </div>
-
+    
           <button 
             type="submit" 
-            className={`w-full py-3.5 rounded-xl font-bold text-white transition-all duration-300 transform hover:scale-[1.02] active:scale-95 shadow-lg
-            ${darkMode ? 'bg-linear-to-r from-cyan-600 to-blue-600 hover:from-cyan-500 hover:to-blue-500 shadow-cyan-900/20' : 'bg-linear-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 shadow-blue-200'}`}
+            onClick={handleLogin}
+            className={`w-full py-3.5 rounded-xl font-bold text-white transition-all duration-300 transform hover:scale-[1.02] active:scale-95 shadow-lg flex items-center justify-center gap-2
+            ${darkMode ? 'bg-cyan-700 hover:bg-cyan-500 shadow-blue-900/20' : 'bg-cyan-600 hover:bg-cyan-700 shadow-blue-200'}`}
           >
-            Get Started
+           {loader? <Loading/> :'Sign In'}
+            <LucideArrowRight size={18} />
           </button>
         </form>
+        <div className="mt-8 text-center border-t pt-6 border-gray-500/10">
+          <p className={`text-sm ${darkMode ? 'text-gray-500' : 'text-gray-500'}`}>
+            Don't have an account? 
+            <button onClick={()=>{navigate('/signup')}} className={`ml-1 font-bold hover:underline ${darkMode ? 'text-blue-400' : 'text-blue-600'}`}>Create one</button>
+          </p>
+        </div>
       </div>
     </div>
-   </>
-  )
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- src/authantications/Signup.tsx
+++ src/authantications/Signup.tsx
@@ -1,12 +1,50 @@
 import MyContext from "../context/CreateContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { LucideUser, LucideMail, LucideLock, LucideUserPlus } from 'lucide-react';
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Alert from "../componenets/partials/Alert";
+import Loading from "../componenets/partials/Loading";
 
 function Signup() {
-  const { darkMode } = useContext(MyContext);
+  const { darkMode, setAlert, alert, loader, setloader, success, setSuccess } = useContext(MyContext);
   const navigate = useNavigate()
-
+  const [name , setName] = useState('')
+  const [email , setEmail] = useState('')
+  const [password , setpassword] = useState('')
+  const [emailBorder, setemailborder] = useState(false)
+  
+  const createAccount = async()=>{
+    try{
+      setloader(true)
+      await axios.post('http://localhost:9000/auth/signup',{
+         name: name,
+            email: email,
+            password: password
+      })
+      
+      setAlert(true)
+      setSuccess('Account Created Successfully')
+    }catch(err){
+      setAlert(true)
+      setSuccess('Your account could not be created')
+    }finally{
+      setloader(false)
+    }
+  }
+  const handleSubmit = ()=>{
+     let inputtextmatch = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+       if(!inputtextmatch.test(email)){
+        setemailborder(true)
+        setAlert(true)
+        setSuccess('Please enter a valid email address')
+        return
+      }
+    createAccount()
+    setName('')
+    setEmail('')
+    setpassword('')
+  }
   const inputStyle = `w-full p-3 pl-10 rounded-xl border outline-none transition-all duration-300 text-sm md:text-base
   ${
     darkMode
@@ -15,10 +53,11 @@ function Signup() {
   }`;
 
   return (
+    <>
+     {alert && <Alert message={success} darkMode={darkMode} />}
     <div className={`min-h-screen flex items-center justify-center p-6 transition-colors duration-500 ${darkMode ? 'bg-gray-950' : 'bg-gray-50'}`}>
       <div className={`w-full max-w-md p-8 rounded-3xl shadow-2xl transform transition-all duration-500 ${darkMode ? 'bg-gray-900 border border-gray-800' : 'bg-white border border-gray-100'}`}>
         
-        {/* Header */}
         <div className="text-center mb-8">
           <div className={`inline-flex p-4 rounded-2xl mb-4 ${darkMode ? 'bg-cyan-500/10 text-cyan-500' : 'bg-blue-500/10 text-blue-600'}`}>
             <LucideUserPlus size={32} />
@@ -30,23 +69,30 @@ function Signup() {
         <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
           <div className="relative">
             <LucideUser className="absolute left-3 top-3.5 text-gray-400" size={18} />
-            <input type="text" placeholder="Full Name" className={inputStyle} minLength={3} required />
+            <input type="text" placeholder="Full Name" onChange={(e)=>{
+              setName(e.target.value)
+            }} value={name} className={inputStyle} minLength={3} required />
           </div>
 
           <div className="relative">
-            <LucideMail className="absolute left-3 top-3.5 text-gray-400" size={18} />
-            <input type="email" placeholder="Email Address" className={inputStyle} required />
+            <LucideMail className={'absolute left-3 top-3.5 text-gray-400'} size={18} />
+            <input type="email" placeholder="Email Address" onChange={(e)=>{
+              setEmail(e.target.value)
+            }} value={email}  className={`${inputStyle} ${emailBorder?'border-red-600':'border-gray-700'}`} required />
           </div>
           <div className="relative">
             <LucideLock className="absolute left-3 top-3.5 text-gray-400" size={18} />
-            <input type="password" placeholder="Password" className={inputStyle} required />
+            <input type="password" placeholder="Password" onChange={(e)=>{
+              setpassword(e.target.value)
+            }} value={password} className={inputStyle} required />
           </div>
           <button 
+          onClick={handleSubmit}
             type="submit" 
-            className={`w-full py-3.5 rounded-xl font-bold text-white transition-all duration-300 transform hover:scale-[1.02] active:scale-95 shadow-lg
-            ${darkMode ? 'bg-linear-to-r from-cyan-600 to-blue-600 hover:from-cyan-500 hover:to-blue-500 shadow-cyan-900/20' : 'bg-linear-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 shadow-blue-200'}`}
+            className={`w-full py-3.5 rounded-xl font-bold text-white transition-all duration-300 transform hover:scale-[1.02] active:scale-95 shadow-lg flex items-center justify-center gap-2
+            ${darkMode ? 'bg-cyan-700 hover:bg-cyan-500 shadow-blue-900/20' : 'bg-cyan-600 hover:bg-cyan-700 shadow-blue-200'}`}
           >
-            Get Started
+           { loader? <Loading/>:'Get Started'}
           </button>
         </form>
 
@@ -58,6 +104,7 @@ function Signup() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
